53caf77e80135a75427df9b672514e5e6ed7f203
SirAlphaa <captainscar117@gmail.com>
2022-03-26 17:20:23 +1300

    Adding my math Quiz to the task 4

--- src/Task04_1/BetterMathQuiz.java
+++ src/Task04_1/BetterMathQuiz.java
@@ -1,8 +1,144 @@
 package Task04_1;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class BetterMathQuiz {
     
     public static void main(String[] args) {
+        int score = 0;
+        int rounds = 0;
+        Scanner uInput = new Scanner(System.in);
+        MathQuiz quiz = new MathQuiz();
+        boolean Looper = true;
+
+        while (Looper) {
+            try {
+                System.out.print("How many rounds would you like to play? | ");
+                rounds = uInput.nextInt();
+                Looper = false;
+            } catch (InputMismatchException e) {
+                System.out.println("please input a number only.");
+                uInput.nextLine();
+            }
+    }
+
+    clearScreen();
+
+    System.out.println("Let the games begin!!\n\n");
+
+    while (rounds != 0) {
         
+        double actualAns = 0.0, userAns = 0.0;
+        actualAns = quiz.newQuestion();
+            
+        Looper = true;
+        while (Looper) {
+            System.out.print("You answer? | ");
+            String input = uInput.next();
+            if (input.trim().equalsIgnoreCase("x")) {
+                clearScreen();
+                System.out.println("Your final Score is | " + score + "\n\n\n\n\n");
+                uInput.close();
+                System.exit(0);                    
+            }
+            try {
+                userAns = Double.parseDouble(input);
+                Looper = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Invaild input, try again lol\n");
+                uInput.nextLine();
+            }
+        }
+            score = score + quiz.checkAnswer(actualAns, userAns);
+            System.out.println("Current Score   | " + score + "\n");
+            rounds--;
+    }
+    uInput.close();
+    clearScreen();
+    System.out.println("\nGame Complete");
+    System.out.println("\nYour final Score is | " + score + "\n\n\n\n\n");
+    }
+
+    public int checkAnswer(Double realAns, Double userAns) {
+
+        int score;
+        double result = realAns - userAns;
+
+        if (result < 0.5 && result > -0.5) {
+            score =+ 10;
+            clearScreen();
+            System.out.println("Correct!");
+        } else {
+            score =- 10;
+            clearScreen();
+            System.out.println("Wrong :(");
+        }
+
+        return score;
+    }
+
+    public int randomNumber (int upperNumber) {
+        
+        int output = 0;
+        Random rand = new Random();
+
+        output = rand.nextInt(upperNumber + 1);
+
+        return output;
+    }
+
+    public double newQuestion () {
+        
+        int number1 = this.randomNumber(100);
+        int number2 = this.randomNumber(100);
+        String operation = "";
+        double answer = 0.0;
+        int swtichInput = this.randomNumber(3) + 1;
+
+        //1 = +     2 = -   3 = /   4 = *
+        switch (swtichInput) {
+            case 1:
+                operation = " + ";
+                answer = number1 + number2;
+                break;
+
+            case 2:
+                operation = " - ";
+                while (number2 > number1) {
+                    number2 = this.randomNumber(100);
+                }
+                answer = number1 - number2;
+                break;
+            
+            case 3:
+                operation = " * ";
+                answer = number1 * number2;
+            
+            case 4:
+                operation = " / ";
+                while (number2 == 0.0) {
+                    number2 = this.randomNumber(100);
+                }
+                while (number1 < number2) {
+                    number1 = this.randomNumber(100);
+                }
+                answer = Double.valueOf(number1) / Double.valueOf(number2);
+
+            default:
+                break;
+        }
+
+        System.out.println(number1 + operation + number2 + " = ");
+
+        return answer;
+    }
+
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
     }
 }
+
+
