65657c9fb4eed633a3c08c72e7e54e7cf40d7e44
SirAlphaa <captainscar117@gmail.com>
2022-03-26 16:19:14 +1300

    Math Quiz works now

--- src/Task03_2/MathQuiz.java
+++ src/Task03_2/MathQuiz.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class MathQuiz {
     
     public static void main(String[] args) {
-        
-       int score = 0;
-       int rounds = 0;
-       Scanner uInput = new Scanner(System.in);
-       MathQuiz quiz = new MathQuiz();
-       boolean Looper = true;
 
-       while (Looper) {
+        int score = 0;
+        int rounds = 0;
+        Scanner uInput = new Scanner(System.in);
+        MathQuiz quiz = new MathQuiz();
+        boolean Looper = true;
+
+        while (Looper) {
             try {
                 System.out.print("How many rounds would you like to play? | ");
                 rounds = uInput.nextInt();
@@ -24,50 +24,51 @@ public class MathQuiz {
                 System.out.println("please input a number only.");
                 uInput.nextLine();
             }
-       }
+    }
 
-       clearScreen();
+    clearScreen();
 
-       System.out.println("Let the games begin!!\n\n");
+    System.out.println("Let the games begin!!\n\n");
 
-       while (rounds != 0) {
+    while (rounds != 0) {
         
-            double actualAns = 0.0, userAns = 0.0;
-            actualAns = quiz.newQuestion();
+        double actualAns = 0.0, userAns = 0.0;
+        actualAns = quiz.newQuestion();
             
-            Looper = true;
-            while (Looper) {
-                System.out.print("You answer? | ");
-                String input = uInput.next();
-                if (input.trim().equalsIgnoreCase("x")) {
-                    clearScreen();
-                    System.out.println("Your final Score is | " + score + "\n\n\n\n\n");
-                    uInput.close();
-                    System.exit(0);                    
-                }
-                try {
-                    userAns = Double.parseDouble(input);
-                    Looper = false;
-                } catch (InputMismatchException e) {
-                    System.out.println("Invaild input, try again lol\n");
-                    uInput.nextLine();
-                }
+        Looper = true;
+        while (Looper) {
+            System.out.print("You answer? | ");
+            String input = uInput.next();
+            if (input.trim().equalsIgnoreCase("x")) {
+                clearScreen();
+                System.out.println("Your final Score is | " + score + "\n\n\n\n\n");
+                uInput.close();
+                System.exit(0);                    
             }
-
+            try {
+                userAns = Double.parseDouble(input);
+                Looper = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Invaild input, try again lol\n");
+                uInput.nextLine();
+            }
+        }
             score = score + quiz.checkAnswer(actualAns, userAns);
             System.out.println("Current Score   | " + score + "\n");
             rounds--;
-
-       }
-       uInput.close();
-       System.out.println("\nGame Complete");
+    }
+    uInput.close();
+    clearScreen();
+    System.out.println("\nGame Complete");
+    System.out.println("\nYour final Score is | " + score + "\n\n\n\n\n");
     }
 
     public int checkAnswer(Double realAns, Double userAns) {
 
         int score;
+        double result = realAns - userAns;
 
-        if ((realAns - userAns) < 0.5) {
+        if (result < 0.5 && result > -0.5) {
             score =+ 10;
             clearScreen();
             System.out.println("Correct!");
@@ -96,9 +97,10 @@ public class MathQuiz {
         int number2 = this.randomNumber(100);
         String operation = "";
         double answer = 0.0;
+        int swtichInput = this.randomNumber(3) + 1;
 
         //1 = +     2 = -   3 = /   4 = *
-        switch (this.randomNumber(3) + 1) {
+        switch (swtichInput) {
             case 1:
                 operation = " + ";
                 answer = number1 + number2;
@@ -118,10 +120,13 @@ public class MathQuiz {
             
             case 4:
                 operation = " / ";
-                while (number2 == 0 && number2 > number1) {
+                while (number2 == 0.0) {
                     number2 = this.randomNumber(100);
                 }
-                answer = number1 / number2;
+                while (number1 < number2) {
+                    number1 = this.randomNumber(100);
+                }
+                answer = Double.valueOf(number1) / Double.valueOf(number2);
 
             default:
                 break;
